a74e825f41649b32e59d23d46415c0db94024876
Iguchi Shota <e.iguchi1124@gmail.com>
2014-12-14 10:03:01 +0000

    twitter/functionos_controllerの作成，メソッドの移動
    homeコントローラで定義していたツイッターの各種機能のメソッドを
    twitter/functionsコントローラに移動させた

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,36 +3,4 @@ class HomeController < ApplicationController
 
   def index
   end
-
-  def timeline
-    @tweets = twitter_client.home_timeline
-  end
-
-  def mentions_timeline
-    @tweets = twitter_client.mentions_timeline
-  end
-
-  def tweet
-    twitter_client.update("@null test")
-    flash[:notice] = "ツイートしました。"
-    redirect_to :root
-  end
-
-  def follow
-    @users = twitter_client.friends
-  end
-
-  def followers
-    @users = twitter_client.followers
-  end
-
-  private
-  def twitter_client
-    Twitter::REST::Client.new do |config|
-      config.consumer_key = Settings.twitter.consumer_key
-      config.consumer_secret = Settings.twitter.consumer_secret
-      config.access_token = current_user.token
-      config.access_token_secret = current_user.secret
-    end
-  end
 end
---
+++ app/helpers/twitter/functions_helper.rb
@@ -0,0 +1,2 @@
+module Twitter::FunctionsHelper
+end
